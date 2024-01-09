24850fa760411f991439d923ba36fb14c5aa4774
Harish Pandey <pandeyharish394@gmail.com>
2024-01-09 18:33:35 +0545

    created admin login endpoints

---
+++ backend/model/adminModel.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const Admin = mongoose.model("Admin", adminSchema);
+
+module.exports = Admin;

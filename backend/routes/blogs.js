75a44f900487d53037f3aeb9b65f5bd57cfd12e4
Harish Pandey <pandeyharish394@gmail.com>
2024-01-16 21:13:53 +0545

    connected createblog to backend

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -2,33 +2,49 @@ const express = require("express");
 const { body, validationResult } = require("express-validator");
 const router = express.Router();
 const { Article } = require("../model/blogsModel");
+const multer = require("multer");
+
+// Multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./images");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}_${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage });
 
 // Validation middleware for creating an article
 const articleValidation = [
   body("title", "Enter a valid title.").isLength({ min: 3 }),
-  body("content", "Please enter a description with at least 20 characters.").isLength({ min: 20 }),
-  body("category", "Enter a valid category with at least 4 characters.").isLength({ min: 4 }),
-  body("tag", "Enter a proper tag with at least 3 characters.").isLength({ min: 3 }),
+  body("content", "Please enter a description with at least 20 characters.").isLength({ min: 2 }),
+  //   body("category", "Enter a valid category with at least 4 characters.").isLength({ min: 4 }),
+  //   body("tag", "Enter a proper tag with at least 3 characters.").isLength({ min: 3 }),
 ];
 
 // POST endpoint to create a new article with validations
-router.post("/create", articleValidation, async (req, res) => {
+router.post("/create", upload.single("image"), articleValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
 
   try {
-    const { title, content, author, category, tag } = req.body;
+    const { title, content, author, tag, category } = req.body; //, category, tag
+
+    // Check if a file was provided
+    const imageUrl = req.file ? req.file.filename : null;
 
     // Create a new article based on the Article model
     const newArticle = await Article.create({
       title,
       content,
       author, // passing the author's ID
-      category, 
-      tag, 
-      
+      category,
+      tag,
+      imageUrl, // Add the image URL to the article
     });
 
     res.status(201).json({ article: newArticle });
